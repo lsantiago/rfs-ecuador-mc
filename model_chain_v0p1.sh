1a5db74b63004a773998550ed6752b8ca8141382
Pallav Kumar Shrestha <pallav-kumar.shrestha@ufz.de>
2020-04-24 16:17:58 +0200

    1 added rfs folder structure reference 2 issue date from system clock

--- model_chain_v0p1.sh
+++ model_chain_v0p1.sh
@@ -63,17 +63,120 @@
 # VERSION: 0.1
 # 
 #=======================================================================
+#
+# Modifications:
+#
+# Pallav Shrestha 		24 Apr 2020 - added model chain script description
+# Pallav Shrestha 		24 Apr 2020 - added rfs folder structure reference
+#
+#=======================================================================
+
+#---------------
+# FOLDER structure
+#---------------
+
+# rfs-ecuador-mc
+	# templates
+		# catamayo_chira
+			# executables
+				# edk
+				# mhm
+				# smi
+			# namelists
+				# edk.nml
+				# mhm.nml, mhm_output.nml, mrm_output.nml, mhm_parameters.nml
+				# main.dat
+			# variogram_parameters
+				# var_param_pre.txt
+				# var_param_tavg.txt
+				# var_param_tmax.txt
+				# var_param_tmin.txt
+			# meteo_headers
+				# header.txt
+			# primal_restart (e.g. end of 2010)
+				# mhm_restart_001.nc
+				# mrm_restart_001.nc
+			# smi_cdf
+				# cdf_info.nc
+	# scripts
+		# rfs-ecuador-mc (git synced!)
+	# setup_mhm
+		# catamayo_chira
+			# gauge
+			# lai
+			# latlon
+			# morph
+	# download_meteo
+		# catamayo_chira
+			# 2011-01-01
+				# radar_precipitation
+				# era5sd
+	# preprocess_meteo
+		# catamayo_chira
+			# 2011-01-01
+				# meteo
+	# execute_edk
+		# catamayo_chira
+			# 2011-01-01
+				# contol
+					# edk.nml (sed modified)
+					# soft link to variogram parameters
+					# soft link to edk executable
+				# output
+	# execute_mhm
+		# catamayo_chira
+			# 2011-01-01
+				# contol
+					# mhm.nml (sed modified)
+					# soft link to mhm_output, mrm_output, mhm_parameters.nml
+					# soft link to mhm executable
+				# output
+				# restart
+					# t1
+					# t2
+	# execute_smi
+		# catamayo_chira
+			# 2011-01-01
+				# contol
+					# main.dat (sed modified)
+					# soft link to cdf_info.nc
+					# soft link to smi executable
+				# output
+	# postprocess
+		# catamayo_chira
+			# 2011-01-01
+				# ... (for future work)
+
+
+
+
+#---------------
+# DATE control
+#---------------
+
+#==> Cron job commands (to do...). Cron job checks the system clock and 
+#	 decides whether to run the rest of the script
+
+# Get issue year and month from system clock
+issueyear=$(date +'%Y')		# 4 digit year
+issuemonth=$(date +'%m')	# 2 digit month
+issueday=$(date +'%d')		# 2 digit day
+
+
+#---------------
+# Initialize
+#---------------
 
 # Vars
-yEnd_edk=2018
-mEnd_edk=11
-dEnd_edk=02
-yStart_mhm=2012
-mStart_mhm=01
-dStart_mhm=01
-yEnd_mhm=2012
-mEnd_mhm=12
-yEnd_mhm=31
+# yEnd_edk=2018
+# mEnd_edk=11
+# dEnd_edk=02
+# yStart_mhm=2012
+# mStart_mhm=01
+# dStart_mhm=01
+# yEnd_mhm=2012
+# mEnd_mhm=12
+# yEnd_mhm=31
 
 
 # Directories
@@ -82,51 +185,54 @@ mhm_dir="./apps/mhm/"
 smi_dir="./apps/smi/"
 
 
-
+#=======================================================================
 # 1 EDK
-# Step 1.1:     Update metereologicla input file
+#=======================================================================
+
+# Step 1.1:     Update metereological input file
 
 # Step 1.2:     Update processing period and get pre.nc
 cd $edk_dir
 
 cd ./pre/
-sed -i -e  "'/yEnd/c yEnd=${yEnd_edk}'" edk.nml
-sed -i -e  "'/mEnd/c mEnd=${mEnd_edk}'" edk.nml
-sed -i -e  "'/dEnd/c dEnd=${dEnd_edk}'" edk.nml
+sed -i -e  "'/yEnd/c yEnd=${issueyear}'" edk.nml
+sed -i -e  "'/mEnd/c mEnd=${issuemonth}'" edk.nml
+sed -i -e  "'/dEnd/c dEnd=${issueday}'" edk.nml
 ./edk
 cdo -f nc4c -z zip_4 copy pre.nc pre_small.nc
 
 # Step 1.3:     Update processing period and get tmin.nc
 cd  ../tmin
-sed -i -e  "'/yEnd/c yEnd=${yEnd}'" edk.nml
-sed -i -e  "'/mEnd/c mEnd=${mEnd}'" edk.nml
-sed -i -e  "'/dEnd/c dEnd=${dEnd}'" edk.nml
+sed -i -e  "'/yEnd/c yEnd=${issueyear}'" edk.nml
+sed -i -e  "'/mEnd/c mEnd=${issuemonth}'" edk.nml
+sed -i -e  "'/dEnd/c dEnd=${issueday}'" edk.nml
 ./edk
 cdo -f nc4c -z zip_4 copy tmin.nc tmin_small.nc
 
-
 # Step 1.4:     Update processing period and get tmax.nc
 cd  ../tmax/
-sed -i -e  "'/yEnd/c yEnd=${yEnd}'" edk.nml
-sed -i -e  "'/mEnd/c mEnd=${mEnd}'" edk.nml
-sed -i -e  "'/dEnd/c dEnd=${dEnd}'" edk.nml
+sed -i -e  "'/yEnd/c yEnd=${issueyear}'" edk.nml
+sed -i -e  "'/mEnd/c mEnd=${issuemonth}'" edk.nml
+sed -i -e  "'/dEnd/c dEnd=${issueday}'" edk.nml
 ./edk
 cdo -f ncd4c -z zip_4 copy tmax.nc tmax_small.nc
 
 # Step 1.5:     Update processing period and get pre.nc
 cd  cd ../tavg/
-sed -i -e  "'/yEnd/c yEnd=${yEnd}'" edk.nml
-sed -i -e  "'/mEnd/c mEnd=${mEnd}'" edk.nml
-sed -i -e  "'/dEnd/c dEnd=${dEnd}'" edk.nml
+sed -i -e  "'/yEnd/c yEnd=${issueyear}'" edk.nml
+sed -i -e  "'/mEnd/c mEnd=${issuemonth}'" edk.nml
+sed -i -e  "'/dEnd/c dEnd=${issueday}'" edk.nml
 ./edk
 cdo -f ncd4c -z zip_4 copy tavg.nc tavg_small.nc
 
 
 
+#=======================================================================
 # 2 MHM
+#=======================================================================
 cd  $mhm_dir
 
-# Step 2.1:     Update restart
+# Step 2.1:     UPDATE RESTART
 # + Step 2.1.1: Link files nc metereological
 cd $mhm_dir/02_input/meteo
 
@@ -136,26 +242,41 @@ ln -s $edk_dir/tmax/tmax_small.nc tmax.nc
 ln -s $edk_dir/tavg/tavg_small.nc tavg.nc
 
 
-# + Step 2.1.2: Backup  content t1 in t0
-cp $mhm_dir/03_output/restart/t1/* $mhm_dir/03_output/restart/t0/
+# + Step 2.1.2: Archive content t1 in t0
+mkdir $mhm_dir/03_output/restart/t0/$issueyear-$issuemonth-$issueday
+mv $mhm_dir/03_output/restart/t1/* $mhm_dir/03_output/restart/t0/$issueyear-$issuemonth-$issueday/
 
-# + Step 2.1.3: Copy content restart t2 to t1
-cp $mhm_dir/03_output/restart/t2/* $mhm_dir/03_output/restart/t1/
+# + Step 2.1.3: Move content restart t2 to t1
+mv $mhm_dir/03_output/restart/t2/* $mhm_dir/03_output/restart/t1/
 
 # + Step 2.2:   Update eval_Per in mhm.nml 
 cd $mhm_dir/01_control/
-sed -i -e  "'/yStart/c eval_Per(1)%yStart=${yStart_mhm}'" mhm.nml
-sed -i -e  "'/mStart/c eval_Per(1)%mStart=${mStart_mhm}'" mhm.nml
-sed -i -e  "'/dStart/c eval_Per(1)%dStart=${dStart_mhm}'" mhm.nml
-sed -i -e  "'/yEnd/c eval_Per(1)%yEnd=${yEnd_mhm}'" mhm.nml
-sed -i -e  "'/mEnd/c eval_Per(1)%mEnd=${mEnd_mhm}'" mhm.nml
-sed -i -e  "'/dEnd/c eval_Per(1)%dEnd=${dEnd_mhm}'" mhm.nml
+sed -i -e  "'/yStart/c eval_Per(1)%yStart=${issueyear}'" mhm.nml
+sed -i -e  "'/mStart/c eval_Per(1)%mStart=${issuemonth}'" mhm.nml
+sed -i -e  "'/dStart/c eval_Per(1)%dStart=${issueday}'" mhm.nml
+sed -i -e  "'/yEnd/c eval_Per(1)%yEnd=${issueyear}'" mhm.nml
+sed -i -e  "'/mEnd/c eval_Per(1)%mEnd=${issuemonth}'" mhm.nml
+sed -i -e  "'/dEnd/c eval_Per(1)%dEnd=${issueday}'" mhm.nml
 
-# + Step 2.2:   Run MHM
+# + Step 2.2:   RUN MHM
 ./mhm
 
+# Step 2.3:     RELOCATE OUTPUT
+mkdir $mhm_dir/03_output/$issueyear-$issuemonth-$issueday
+mv $mhm_dir/03_output/*.nc $mhm_dir/03_output/$issueyear-$issuemonth-$issueday/
 
+
+
+
+#=======================================================================
 # 3 SMI
+#=======================================================================
+
 # Step 3.1: Run SMI
 cd $smi_dir
 ./smi
+
+
+
+
+
